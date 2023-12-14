790ff5485f8522ccd993faddcc7dfb0980fa62a7
YousufR8 <Yrehman06@gmail.com>
2023-12-14 12:57:56 -0500

    Add project files.

---
+++ Data Sorting - BPA Prep 1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace CSVColumnSplitter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\rehman_yousuf\source\repos\Data Sorting - BPA Prep 1\Data Sorting - BPA Prep 1\Sample_Data_Age.csv";
+
+            if (File.Exists(filePath))
+            {
+                try
+                {
+                    string[] lines = File.ReadAllLines(filePath);
+
+                    foreach (string line in lines)
+                    {
+                        string[] columns = line.Split(',');
+                        foreach (string column in columns)
+                        {
+                            Console.Write($"{column.Trim()} | ");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"File not found: {filePath}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
